27c0311435020e8ef47fcdc5acb1bd88a6e2d587
kommi venu <kommivenu93@gmail.com>
2023-07-10 11:46:09 +0530

    first commit

---
+++ IfElseStatementExample1.java
@@ -0,0 +1,17 @@
+//A Java Program to demonstrate the use of if-else statement.  
+//It is a program of odd and even number.  
+public class IfElseStatementExample1 {  
+public static void main(String[] args) {  
+    //defining a variable  
+    int number=17;  
+    //Check if the number is divisible by 2 or not  
+    if(number%2==0){  
+        System.out.println("even number");  
+    }
+	
+	else{  
+        System.out.println("odd number");  
+    }  
+	System.out.println("This statement is outside the if-else block.");
+}  
+}  
